1c8a1e1899b8235ca27cbe367c004caea4451100
k022g0028@g.neec.ac.jp <k022g0028@g.neec.ac.jp>
2023-05-31 13:04:08 +0900

    シーン

--- Assets/Script/SceneTransition.cs
+++ Assets/Script/SceneTransition.cs
@@ -23,39 +23,38 @@ public class SceneTransition : MonoBehaviour
             if (player != null)
             {
                 playerScript = player.GetComponent<Player>();
-                if (playerScript != null )
+                if (playerScript != null)
                 {
-                    if(player.transform.position.x >= 4.4f)
+                    if (player.transform.position.x >= 4.4f)
                     {
                         SceneManager.LoadScene("Scenes/SampleScene");
                     }
                 }
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (SceneManager.GetActiveScene().name == "SampleScene")
         {
-            if (SceneManager.GetActiveScene().name == "SampleScene")
+            GameManager gameManagerScript;
+            GameObject gameManager = GameObject.Find("GameManager");
+            if (gameManager != null)
             {
-                GameManager gameManagerScript;
-                GameObject gameManager = GameObject.Find("GameManager");
-                if (gameManager != null)
+                gameManagerScript = gameManager.GetComponent<GameManager>();
+                if (gameManagerScript != null)
                 {
-                    gameManagerScript = gameManager.GetComponent<GameManager>();
-                    if (gameManagerScript != null)
+                    if (gameManagerScript.isClear)
                     {
-                        if (gameManagerScript.isClear)
-                        {
-                            SceneManager.LoadScene("Scenes/GameClear");
-                        }
-                        else if (gameManagerScript.isGameOver)
-                        {
-                            SceneManager.LoadScene("Scenes/GameOver");
-                        }
+                        SceneManager.LoadScene("Scenes/GameClear");
+                    }
+                    else if (gameManagerScript.isGameOver)
+                    {
+                        SceneManager.LoadScene("Scenes/GameOver");
                     }
                 }
             }
-            else if(SceneManager.GetActiveScene().name == "GameClear" ||
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (SceneManager.GetActiveScene().name == "GameClear" ||
                 SceneManager.GetActiveScene().name == "GameOver")
             {
                 SceneManager.LoadScene("Scenes/Title");
